00020beb6f59a296e3ff025933efb5e354622947
Jacky Lee <leeja12@oocl.com>
2024-11-27 15:24:35 +0800

    fix: add index key for map

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -6,8 +6,8 @@ export default function ProductCategory({productCategory}) {
   return (
     <div style={{width: "100%"}}>
         <span className="product-name">{productCategory.name}</span>
-        {productCategory.productItems.map((productItem) => {
-            return <ProductItem productItem={productItem}/>
+        {productCategory.productItems.map((productItem, index) => {
+            return <ProductItem productItem={productItem} key={productItem.name + index}/>
         })}
     </div>
   )
